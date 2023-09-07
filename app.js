825f7445be68e5bbb3594d551b864b5b3abd6db5
M-Parsaei <khodadostetdarm@gmail.com>
2023-09-06 20:22:21 -0700

    added code for parsing body requests

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
+import products from './data';
 
-const port = process.env.PORT || 3000 ;
+// Parsing the request bodies 
+app.use(express.urlencoded({extended:true}));
+// In order to parse JSON  files for body requests
+app.use(express.json());
+
 
 
 mongoose.connect(process.env.MONGODBURL)
@@ -14,7 +19,13 @@ mongoose.connect(process.env.MONGODBURL)
 app.get('/', (req,res) => {
     res.send('Hello');
 })
+app.post('/addproducts',(req,res)=>{
+    
+})
 
-app.listen(port, () =>{
+// For all other invalid url, sending a 404 status.
+app.use('*',(req,res)=>{ res.status(404).send('Doesn\'t exist 404.')})
+
+app.listen(process.env.PORT || 3000, () =>{
     console.log('Server is running');
 })
---
+++ data.js
@@ -0,0 +1,11 @@
+const products = [
+    {
+        name: 'Gurkirat',
+        year: '2',
+    },
+    {
+        name: 'Mohammad',
+        year: '3'
+    },
+]
+export default products;
